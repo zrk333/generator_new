7f5b8f31f717f7a7030cafd6cb13a9cfbc7a9eaf
zrk <492407403@qq.com>
2020-08-27 21:09:32 +0800

    Initial commit

---
+++ src/main/java/com/platform/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.platform.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author zrk
+ * @Date 2020/8/25
+ */
+@RestController
+public class TestController {
+
+//    @Resource
+//    private ActionMapper actionMapper;
+
+//    @RequestMapping
+//    public Integer aa(){
+//        Action action = new Action();
+//        action.getId();
+//        return actionMapper.insert(new Action());
+//    }
+}
